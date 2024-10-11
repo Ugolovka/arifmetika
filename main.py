f8db489e69041ae2a3a6e9e8bae747ca9c8885b7
Ugolovka <Medusa_Gangrena@tutamail.com>
2024-10-11 17:27:14 +0300

    не доделал

---
+++ main.py
@@ -0,0 +1,18 @@
+input_data = open("input.txt","r")
+data = input_data.read()
+data =data.split()
+a = int(data[0])
+b = int(data[1])
+c = int(data[2])
+(set(str(a , b , c)))
+if a >= b and a >= c:
+    more = a
+elif b >= a and b>= c:
+    more = b
+elif c >= a and c>= b:
+    more = c
+more = str(more)
+output_data = open("output.txt","w")
+output_data.write(more)
+input_data.close()
+output_data.close()
